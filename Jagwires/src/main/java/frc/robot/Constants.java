6aeda2534defa9a239f4c9e959d509f1f36e9cee
pradeepravi26 <pradeepravi2006@gmail.com>
2023-02-15 16:27:45 -0500

    changed xbox controller to generichid controller

--- Jagwires/src/main/java/frc/robot/Constants.java
+++ Jagwires/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,6 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  
+  public static final int kJoystickPort = 0;
 }
