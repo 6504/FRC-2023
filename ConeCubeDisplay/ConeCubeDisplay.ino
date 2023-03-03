ef4f48b1812659ba4b94141319a9be678b38d8d0
boothinator <boothinator@users.noreply.github.com>
2023-03-02 22:10:32 -0500

    Update ConeCubeDisplay.ino

--- ConeCubeDisplay/ConeCubeDisplay.ino
+++ ConeCubeDisplay/ConeCubeDisplay.ino
@@ -2,11 +2,32 @@
 
 #define NUM_LEDS 64
 CRGB leds[NUM_LEDS];
+
+char mode = 'b';
+
 void setup() {
+  Serial.begin(9600);
+
+  pinMode(MISO, OUTPUT);
 
   FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
-  FastLED.setBrightness(20);
+  FastLED.setBrightness(255);
+  
+  // limit my draw to 1A at 5v of power draw
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500); 
+  
+  // turn on SPI in slave mode
+  SPCR |= _BV(SPE);
+  
+  // turn on interrupts
+  SPCR |= _BV(SPIE);
 
+}
+
+// SPI interrupt routine
+ISR (SPI_STC_vect)
+{
+  mode = SPDR;
 }
 
 constexpr unsigned int getIndex(unsigned int col, unsigned int row)
@@ -14,12 +35,17 @@ constexpr unsigned int getIndex(unsigned int col, unsigned int row)
   return ((row & 1 == 1) ? 7 - col : col) + row * 8;
 }
 
-void clear()
+void setAll(CRGB color)
 {
   for (int i = 0; i < NUM_LEDS; i++)
   {
-    leds[i] = CRGB::Black;
+    leds[i] = color;
   }
+}
+
+void clear()
+{
+  setAll(CRGB::Black);
 }
 
 void showCube(CRGB color)
@@ -31,11 +57,15 @@ void showCube(CRGB color)
     leds[getIndex(0, i)] = color;
     leds[getIndex(7, i)] = color;
   }
-  FastLED.show();
 }
 
-void loop() {
-  clear();
+void showRainbow()
+{
+  fill_rainbow(leds, NUM_LEDS, 0, NUM_LEDS/8);
+}
+
+void showCone()
+{
   leds[getIndex(3, 0)] = CRGB::Yellow;
   leds[getIndex(4, 0)] = CRGB::Yellow;
   
@@ -69,15 +99,71 @@ void loop() {
   {
     leds[getIndex(i, 7)] = CRGB::Yellow;
   }
-  FastLED.show();
+}
 
-  delay(2000);
+void loop() {
+  static unsigned long lastFrameMillis = millis();
 
-  clear();
-  showCube(CRGB::Red);
-  delay(2000);
+  if ('c' == mode)
+  {
+    // Cycle
+    static int i = 0;
 
-  clear();
-  showCube(CRGB::Blue);
-  delay(2000);
+    if (millis() - lastFrameMillis > 2000) {
+      lastFrameMillis = millis();
+      i++;
+      if (i > 2) {
+        i = 0;
+      }
+    }
+    
+    clear();
+    if (i == 0)
+    {
+      showCone();
+    } else if (i == 1)
+    {
+      showCube(CRGB::Red);
+    } else if (i == 2)
+    {
+      showCube(CRGB::Blue);
+    }
+    FastLED.show();
+  } else if ('R' == mode) {
+    // Red Cube
+    clear();
+    showCube(CRGB::Red);
+    FastLED.show();
+  } else if ('B' == mode) {
+    // Blue Cube
+    clear();
+    showCube(CRGB::Blue);
+    FastLED.show();
+  } else if ('C' == mode) {
+    // Cone
+    clear();
+    showCone();
+    FastLED.show();
+  } else if ('G' == mode) {
+    // Green
+    setAll(CRGB::Green);
+    FastLED.show();
+  } else if ('b' == mode) {
+    clear();
+    FastLED.show();
+  } else if ('r' == mode) {
+    showRainbow();
+    FastLED.show();
+  }
+
+  if (Serial.available())
+  {
+    char c;
+    c = Serial.read();
+    if (c != '\n' && c != '\r')
+    {
+      mode = c;
+      Serial.println(mode);
+    }
+  }
 }
