b22c1e63851ded142ccb6f482485814ebba96bda
fallschurchhsrobotics <61054433+fallschurchhsrobotics@users.noreply.github.com>
2023-03-14 15:57:30 -0400

    Auto w/ dist

--- Jagwires/src/main/java/frc/robot/Constants.java
+++ Jagwires/src/main/java/frc/robot/Constants.java
@@ -42,7 +42,7 @@ public final class Constants {
 
   public static final double kHighLift = 65.0;
 
-  public static final double kMidLift = 40.0;
+  public static final double kMidLift = 50.0;
  
   public static final double kLowLift = 20.0;
 
